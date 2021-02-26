74cefcf4e9f2becda3e5fd7661273484acb354f1
Мария Пономаренко <muchana96@mail.ru>
2021-02-26 19:51:34 +0400

    no message

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
-var body = document.body;
 let url = window.location.href;
-let getName = (url) =>  {
-  let g = url.split('=');
-  let name = g[1];
-  if (name == undefined) {
-      name = 'saifieva98'
+let nameGenerator = (url) => {
+  let a = url.split('=');
+  let name = a[1];
+  if (name == undifined) {
+    name = 'ponomarenko-m';
   }
   return name;
 }
 
-fetch(`https://api.github.com/users/${getName(url)}`)
+fetch(`https:api.github.com/users/${nameGenerator(url)}`)
   .then(res => res.json())
   .then(json => {
     let name = document.createElement('h1');
     if (json.name != null) {
-         name.innerHTML = json.name;
-      } else {
-         name.innerHTML = 'Данные отсутствуют';
-      }
-    body.append(name);
-    name.addEventListener("click", () => window.location = json.html_url);
+      name.innerHTML = json.name;
+    } else {
+      name.innerHTML = 'Данные отсутствуют';
+    }
+    document.body.append(name);
+    name.addEventListener('click', () => window.location = json.html_url);
+
     let description = document.createElement('p');
-    if (json.bio != null) {
-         description.innerHTML = json.bio;
-      } else {
-         description.innerHTML = 'Данные отсутствуют';
-      }
-    body.append(description);
-    let img = new Image();
-    img.src = json.avatar_url;
-    body.append(img);
+    if(json.bio != null) {
+      description.innerHTML = 'Данные отсутствуют';
+    }
+    document.body.append(description);
+
+    let image = new Image();
+    image.src = json.avatar_url;
+    document.body.append(image);
   })
+
   .catch(err => console.log(err));
